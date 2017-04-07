8e12447b235f6efbeff48da25a179e01d944c5b6
Lennart Bramlage <lennart.bramlage@tuebingen.mpg.de>
2017-04-07 17:17:56 +0200

    trying to convert js image data string to mat.imagedata and back

--- example/ocv.cpp
+++ example/ocv.cpp
@@ -25,6 +25,20 @@ uint8_t put_picture(){
     return *img.data;
 }
 
+int* mat_array(const cv::Mat img) {
+  int input[img.cols * img.rows];
+  unsigned char* shinput = (unsigned char *)(img.data);
+  cout << sizeof(input) << " " << img.channels() << endl;
+  for (int i = 0, j = 0; i < sizeof(input); i += img.channels(), j +=4){
+    input[j] = (int)img.data[i];
+    input[j + 1] = (int)img.data[i+1%img.channels()];
+    input[j + 2] = (int)img.data[i+2%img.channels()];
+    input[j + 3] = 255;
+    // cout << input[j] << " " << input[j+1] << " " << input[j+2] << " " << input[j+3] << " " << endl;
+  }
+  return input;
+}
+
 string mat_stuff(int width, int height, string img_data){
     std::vector<uint8_t> img_vector(img_data.begin(), img_data.end());
     cv::Mat img = cv::Mat(height, width, CV_8UC1, &img_vector);
@@ -36,8 +50,13 @@ string mat_stuff(int width, int height, string img_data){
     ptrSBD->detect(img, keypoints);
     cv::Mat im_with_keypoints;
     drawKeypoints( img, keypoints, im_with_keypoints, cv::Scalar(0,0,255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
-    return img_data;
+
+    ostringstream ss;
+    int* img_array = mat_array(im_with_keypoints);
+    copy(img_array, img_array+(im_with_keypoints.rows * im_with_keypoints.cols * im_with_keypoints.channels()), ostream_iterator<int>(ss, ","));
+    return ss.str();
 }
+
 
 EMSCRIPTEN_BINDINGS(my_module) {
     emscripten::function("cool", &cool);
