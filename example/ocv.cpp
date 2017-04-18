97c3dc6b5ea649ac100eb322335368afc1e88b6e
Lennart Bramlage <lennart.bramlage@tuebingen.mpg.de>
2017-04-18 17:41:51 +0200

    fixed broken pointer to img data in mat creation

--- example/ocv.cpp
+++ example/ocv.cpp
@@ -21,7 +21,16 @@ string int_array_to_string(int int_array[], int size_of_array) {
 }
 
 string mat_to_array(int width, int height, string img_data){
-  vector<uchar> img_vector(img_data.begin(), img_data.end());
+  vector<uchar> img_vector;
+  std::stringstream ss(img_data);
+  int i;
+  while (ss >> i) {
+      img_vector.push_back(i);
+
+      if (ss.peek() == ',')
+          ss.ignore();
+  }
+  cout << img_vector[2] << endl;
   cv::Mat img = cv::Mat(height, width, CV_8UC3, &img_vector);
 
   // cout << "CHANNELS: " << img.channels() << endl;
@@ -44,31 +53,41 @@ string mat_to_array(int width, int height, string img_data){
   return img_data; // str;
 }
 
-int* mat_array(const cv::Mat img) {
-  int input[img.cols * img.rows];
-  for (int i = 0, j = 0; i < sizeof(input); i += img.channels(), j +=4){
-    input[j] = (int)img.data[i];
-    input[j + 1] = (int)img.data[i+1%img.channels()];
-    input[j + 2] = (int)img.data[i+2%img.channels()];
-    input[j + 3] = 255;
+// int* mat_array(const cv::Mat img) {
+//   int input[img.cols * img.rows];
+//   for (int i = 0, j = 0; i < sizeof(input); i += img.channels(), j +=4){
+//     input[j] = (int)img.data[i];
+//     input[j + 1] = (int)img.data[i+1%img.channels()];
+//     input[j + 2] = (int)img.data[i+2%img.channels()];
+//     input[j + 3] = 255;
+//   }
+//   return input;
+// }
+//
+// string mat_string(int width, int height, string img_data){
+//   std::vector<uint8_t> img_vector(img_data.begin(), img_data.end());
+//   cv::Mat img = cv::Mat(height, width, CV_8UC1, &img_vector);
+//   ostringstream ss;
+//   int* img_array = mat_array(img);
+//   copy(img_array, img_array+(img.rows * img.cols * img.channels()), ostream_iterator<int>(ss, ","));
+//   return ss.str();
+// }
+//
+// string mat_to_string(cv::Mat img) {
+//   ostringstream ss;
+//   int* img_array = mat_array(img);
+//   copy(img_array, img_array + (img.rows * img.cols * img.channels()), ostream_iterator<int>(ss, ","));
+//   return ss.str();
+// }
+
+cv::Mat test_img(const int height, const int width) {
+  cv::Mat img = cv::Mat::zeros(height, width, CV_8UC3);
+  for(int i = 0; i < height; i++){
+    for(int j = 0; j < height; j++){
+      img.at<cv::Vec3b>(i, j)[2] = 255;
+    }
   }
-  return input;
-}
-
-string mat_string(int width, int height, string img_data){
-  std::vector<uint8_t> img_vector(img_data.begin(), img_data.end());
-  cv::Mat img = cv::Mat(height, width, CV_8UC1, &img_vector);
-  ostringstream ss;
-  int* img_array = mat_array(img);
-  copy(img_array, img_array+(img.rows * img.cols * img.channels()), ostream_iterator<int>(ss, ","));
-  return ss.str();
-}
-
-string mat_to_string(cv::Mat img) {
-  ostringstream ss;
-  int* img_array = mat_array(img);
-  copy(img_array, img_array + (img.rows * img.cols * img.channels()), ostream_iterator<int>(ss, ","));
-  return ss.str();
+  return img;
 }
 
 std::vector<int> mat_vector(const cv::Mat img){
@@ -82,66 +101,57 @@ std::vector<int> mat_vector(const cv::Mat img){
   return input;
 }
 
-string vector_to_string(std::vector<int> img_vector){
-  //clumsy
-  string returnstring = "";
-  for(int i = 0; i < img_vector.size(); i++){
-    returnstring += to_string(img_vector[i]);
-    returnstring += ',';
-  }
-  return returnstring;
-}
-
 cv::Mat string_to_mat(int height, int width, std::string img_data){
-  vector<uchar> img_vector(img_data.begin(), img_data.end());
-  cv::Mat img = cv::Mat(height, width, CV_8UC3, &img_vector);
+  vector<uchar> img_vector;
+  std::stringstream ss(img_data);
+  int i;
+  while (ss >> i) {
+    img_vector.push_back(i);
+
+    if (ss.peek() == ',')
+        ss.ignore();
+  }
+  // for(int j = 0; j < img_vector.size(); j++){
+  //   cout << (int)img_vector[j] << endl;
+  // }
+  cv::Mat img = cv::Mat(height, width, CV_8UC3, img_vector.data());
+  // memcpy(img.data, img_vector.data(), img_vector.size()*sizeof(uchar));
   return img;
 }
 
-std::string v_to_s(const std::vector<int> img_vector) {
-  //this needs to be split by ' ' in js
+std::string vector_to_string(const std::vector<int> img_vector) {
+  // this needs to be split by ' ' in js
+  // it should be js compatible
   std::stringstream result;
   std::copy(img_vector.begin(), img_vector.end(), std::ostream_iterator<int>(result, ","));
   return (result.str()).substr(0, (result.str()).size() - 1);
 }
 
-cv::Mat test_img(const int height, const int width) {
-  cv::Mat img = cv::Mat::zeros(height, width, CV_8UC3);
-  for(int i = 0; i < height; i++){
-    for(int j = 0; j < height; j++){
-      img.at<cv::Vec3b>(i, j)[2] = 255;
-    }
-  }
-  return img;
-}
-
 int main(){
-  cv::imwrite("other_test.jpg", test_img(20,20));
-  cv::Mat img = cv::imread("other_test.jpg");
+  cv::imwrite("img_in.jpg", test_img(1000, 1000));
+  cv::Mat img = cv::imread("img_in.jpg", CV_LOAD_IMAGE_ANYDEPTH | CV_LOAD_IMAGE_ANYCOLOR);
 
-  cout << "It does compile" << endl;
   cout << "Rows: " << img.rows << endl;
   cout << "Columns: " << img.cols << endl;
   cout << "Channels: " << img.channels() << endl;
-  // string s = mat_to_array(img.rows, img.cols, img_data_string);
 
   // img to vector
   std::vector<int> img_vector = mat_vector(img);
 
   // vector to string
-  string img_data_string = v_to_s(img_vector);
+  string img_data_string = vector_to_string(img_vector);
 
   // string to mat
-  cv::Mat stringed_mat = string_to_mat(img.rows, img.cols, img_data_string);
+  cv::Mat processed_mat = string_to_mat(img.rows, img.cols, img_data_string);
 
   // save mat to file
-  cv::imwrite("weird.jpg", stringed_mat);
+  cv::imwrite("img_out.jpg", processed_mat);
 
   cv::Mat gray_image;
-  cvtColor(img, gray_image, cv::COLOR_BGR2GRAY);
+  cvtColor(processed_mat, gray_image, CV_BGR2GRAY);
 
   cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE );
-  cv::imshow( "Display window", stringed_mat );
+  cv::imshow( "Display window", gray_image );
 
   cv::waitKey(0);
   return 0;
