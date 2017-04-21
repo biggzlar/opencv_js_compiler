0295a97b8bb75013b66df061ccef998f62272cc5
Lennart Bramlage <lennart.bramlage@tuebingen.mpg.de>
2017-04-21 11:48:09 +0200

    cleanup, simplified js_to_mat, mat_to_js

--- example/js_example.cpp
+++ example/js_example.cpp
@@ -11,16 +11,8 @@
 
 using namespace std;
 
-string int_array_to_string(int int_array[], int size_of_array) {
-  string returnstring = "";
-  for (int temp = 0; temp < size_of_array; temp++) {
-    returnstring += to_string(int_array[temp]);
-    returnstring += ',';
-  }
-  return returnstring;
-}
-
 cv::Mat test_img(const int height, const int width) {
+  // Creates a test image of a given size height*width
   cv::Mat img = cv::Mat::zeros(height, width, CV_8UC3);
   for(int i = 0; i < height; i++){
     for(int j = 0; j < height; j++){
@@ -30,35 +22,24 @@ cv::Mat test_img(const int height, const int width) {
   return img;
 }
 
-std::vector<int> mat_vector(const cv::Mat img){
-  // assumed it should be (img.cols * img.rows * img.channels()) but js wants a
-  // 4th value (255 for alpha channel?) this could be problematic with mat computations
-  // we might need to add this in at a later point (string generation maybe)
+std::string mat_to_js(const cv::Mat img) {
+  std::vector<int> img_vector(img.cols * img.rows * 4);
 
-  // this is where image data is submitted from context.createimagedata(), so it
-  // probably, actually has 4 channels?
-  std::vector<int> input(img.cols * img.rows * 4);
-  // cout << "IMAGE COLS: " << img.cols << endl;
-  // cout << "IMAGE ROWS: " << img.rows << endl;
-  // cout << "IMAGE CHANNELS: " << img.channels() << endl;
-  // cout << "SIZE OF INPUT: " << input.size() << endl;
-
-  // this will be the most difficult to get straight with the varying number of
-  // channels
-  for (int i = 0, j = 0; j < input.size() - 4; i += 4, j += 4){
-    input[j] = (int)img.data[i];
-    input[j + 1] = (int)img.data[i+1%4];
-    input[j + 2] = (int)img.data[i+2%4];
-    input[j + 3] = 255;
+  for (int i = 0, j = 0; j < img_vector.size() - 4; i += 4, j += 4){
+    img_vector[j]     = (int)img.data[i];
+    img_vector[j + 1] = (int)img.data[i+1%4];
+    img_vector[j + 2] = (int)img.data[i+2%4];
+    img_vector[j + 3] = 255;
   }
-  return input;
+
+  std::stringstream result;
+  std::copy(img_vector.begin(), img_vector.end(), std::ostream_iterator<int>(result, ","));
+  return (result.str()).substr(0, (result.str()).size() - 1);
 }
 
 cv::Mat string_to_mat(int height, int width, std::string img_data){
   // splits up a ',' delimited string into a vector of unsigned chars, then
   // attempts to create a cv::Mat from it
-
-  // img_data / height / width should be the number of input channels, no?
   vector<uchar> img_vector;
   std::stringstream ss(img_data);
 
@@ -70,19 +51,8 @@ cv::Mat string_to_mat(int height, int width, std::string img_data){
         ss.ignore();
   }
 
-  int number_of_channels = img_vector.size() / height / width;
-  cout << "IMG_VECTOR.SIZE(): " << img_vector.size() << endl;
-  cout << "CHANNEL GUESS: " << number_of_channels << endl;
-
   cv::Mat img = cv::Mat(height, width, CV_8UC4, img_vector.data());
   return img;
-}
-
-std::string vector_to_string(const std::vector<int> img_vector) {
-  // this needs to be split by ',' in js
-  std::stringstream result;
-  std::copy(img_vector.begin(), img_vector.end(), std::ostream_iterator<int>(result, ","));
-  return (result.str()).substr(0, (result.str()).size() - 1);
 }
 
 string type2str(int type) {
@@ -108,22 +78,24 @@ string type2str(int type) {
   return r;
 }
 
-std::string eternal_return(int height, int width, std::string img_data) {
+std::string js_return(int height, int width, std::string img_data) {
   cv::Mat img = string_to_mat(height, width, img_data);
-  cv::Mat processed_mat;
+  cv::Mat processed_mat, final_destination;
 
-  blur(img, processed_mat, cv::Size(16,16));
-  // cvtColor(img, processed_mat, CV_RGBA2GRAY);
-  // cvtColor(processed_mat, img, CV_GRAY2RGBA);
+  // blur(img, processed_mat, cv::Size(16,16));
+  cvtColor(img, processed_mat, CV_RGBA2GRAY);
+  cvtColor(processed_mat, processed_mat, CV_GRAY2RGBA);
+  threshold(processed_mat, final_destination, 97, 255, 0);
+
   cout << "PROCESSED MAT" << endl;
   cout << "TYPE: " << type2str(processed_mat.type()) << endl;
   cout << "CHANNELS: " << processed_mat.channels() << endl;
 
-  std::string mat_string = vector_to_string(mat_vector(processed_mat));
+  std::string mat_string = mat_to_js(final_destination);
 
   return mat_string;
 }
 
 EMSCRIPTEN_BINDINGS(my_module) {
-    emscripten::function("eternal_return", &eternal_return);
+    emscripten::function("js_return", &js_return);
 }
